faf1e6572c0a7a6b626db3c7efb41335b2adb8e7
Michael Janum Davidsen <au600586@uni.au.dk>
2020-10-20 11:41:20 +0200

    Databinding på AddDebtorCreditor

--- MandatoryAssignment1/DebtBook/ViewModels/AddDebtorCreditorWindowViewModel.cs
+++ MandatoryAssignment1/DebtBook/ViewModels/AddDebtorCreditorWindowViewModel.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Prism.Mvvm;
 using DebtBook.Models;
+using Prism.Commands;
 
 namespace DebtBook.ViewModels
 {
@@ -14,6 +17,11 @@ namespace DebtBook.ViewModels
         private string name;
         private double debit;
 
+        public AddDebtorCreditorWindowViewModel() : this(new DebtorCreditor(), string.Empty, double.Parse(string.Empty))
+        {
+
+        }
+
         public AddDebtorCreditorWindowViewModel(DebtorCreditor debtor, string name, double debit)
         {
             this.debtor = debtor;
@@ -22,14 +30,53 @@ namespace DebtBook.ViewModels
         }
 
 
+        #region Properties
+
+        public string newName
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        public double newDebit
+        {
+            get => debit;
+            set => SetProperty(ref debit, value);
+        }
+
+
+        #endregion Properties 
+
+        #region Commands
+
+        public ICommand _AddButtonCommand;
+
+        public ICommand AddButtonCommand
+        {
+            get
+            {
+                return _AddButtonCommand ??
+                       (_AddButtonCommand = new DelegateCommand(
+                               AddButtonCommand_Execute, AddButtonCommand_CanExecute)
+                           .ObservesProperty(() => newName)
+                           .ObservesProperty(() => newDebit));
+            }
+            //set => throw new NotImplementedException();
+        }
+
+        private void AddButtonCommand_Execute()
+        {
+            //do nothing
+        }
+
+        private bool AddButtonCommand_CanExecute()
+        {
+            return true;
+        }
+
+        #endregion Commands
 
     }
 
-    #region Properties
 
-    #endregion Properties 
-
-    #region Commands
-
-    #endregion Commands
 }
